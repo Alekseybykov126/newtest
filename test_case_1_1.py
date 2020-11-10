740b43ef1299128015e06812a7fcb2f1be09c8a8
aleh126 <70600596+Alekseybykov126@users.noreply.github.com>
2020-11-10 12:16:30 +0300

    Initial commit

---
+++ test_case_1_1.py
@@ -0,0 +1,39 @@
+from .page import *
+
+def case_1_1(self, fullscreen):
+
+    #self.page.loger("\n Запуск Тест кейс № 1_1 tvweb_new-1_1: авторизация \n")
+    print("\n Запуск Тест кейс № 1_1 tvweb_new-1_1: авторизация \n")
+    time.sleep(2)
+
+    self.page.login_matchtv('9776410337', 'Alekseybykov126')
+    self.page.loger('Авторизация прошла успешно')
+
+    resic = str(self.result.find_link("a", "reset-link pm-gate__button"))
+    assert ('Выход') in resic
+    self.page.loger('Кнопка "Вход" сменилась на "Выход"')
+
+    self.page.loger('Шаг 5. Клик по иконке пользователя')
+    self.driver.find_element_by_xpath('.//a[@href="/profile"]').click()
+    time.sleep(2)
+
+    self.page.waitForElementVisible('.//h3[@class="profile-header__item heading heading-3"]', 5)
+    self.page.loger('Осуществлён переход в личный кабинет')
+    time.sleep(1)
+
+    self.page.loger('Шаг 6. Клик по кнопке "Выход"')
+    self.driver.find_element_by_xpath('.//a[@href="/pm/logout"]').click()
+    time.sleep(1)
+
+    self.driver.waitForElementVisible('.//div[@class="form logout-form"]')
+    self.page.loger('Появилась форма "Выйти из аккаунта?"')
+
+    self.page.loger('Шаг 7. Подтверждение выхода из акаунта')
+    self.driver.find_element_by_xpath('.//button[@type="button"]').click()
+    time.sleep(1)
+
+    resic = str(self.result.find_link("span", "pm-gate__label"))
+    assert ('Вход') in resic
+    self.page.loger('Кнопка "Выход" сменилась на "Вход"')
+
+    self.driver.quit()
---
+++ test_suit.py
@@ -0,0 +1,26 @@
+from .page import *
+from .test_case_1_1 import case_1_1
+
+
+class Test:
+    def setup(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--incognito')
+        self.driver = webdriver.Chrome(options=options)
+        #self.driver = webdriver.Chrome('C:/Users/abykov.RUTUBE/Desktop/newtest/chromedriver.exe')
+        self.driver.get("https://www.matchtv.ru/")
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.full_screen = 0
+        self.page = MainPage(self.driver)
+        self.result = ResultPage(self.driver)
+        self.prof = Profile(self.driver)
+        self.admin = Admin(self.driver)
+        self.card = CardFilm(self.driver)
+
+    # def teardown(self):
+    #     self.driver.quit()
+
+
+    def test_case_1_1(self):
+        case_1_1(self, 0)
